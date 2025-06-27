521eea4c4bb8f1b6b7efe75ae224ab12f80f5642
Isa Scollo <iscollo4@gmail.com>
2025-06-27 11:19:30 +1000

    initial commit

---
+++ extract_order.js
@@ -0,0 +1,13 @@
+"use strict";
+// ** Write your module here **
+// It must send an event "order_details" from the page containing an Order object,
+// which describes all the relevant data points on the page.
+// The order_details data you are sending should match the `expected_output` object in `test.js`
+
+module.exports = function extract_order() {
+  try {
+    // Scrape order details and Create Order object.
+  } catch (e) {
+    console.error(e);
+  }
+};
